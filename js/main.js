0aad13ebdb972a6c255970d3f3a448435b3d6d29
ARISTAHS <paran0907@naver.com>
2024-06-20 20:33:57 +0900

    카카오 지도 연동 및 마커 표시 작업

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-// swiper js 
 window.onload = function(){
+
+  // swiper js 
   let swiper = new Swiper(".mySwiper", {
     scrollbar: {
       el: ".swiper-scrollbar",
@@ -13,5 +14,27 @@ window.onload = function(){
     },
     speed : 400,
   });
-};
 
+  // 카카오 map API 
+  kakao.maps.load(function() {
+    let container = document.getElementById(`club_map`);
+    let option = {
+      center : new kakao.maps.LatLng(37.502047 , 127.107505),
+      level : 3
+    };
+    let club_map = new kakao.maps.Map(container, option);
+
+    // 지도에 마커 표시
+    let markerPoint = new kakao.maps.LatLng(37.502047 , 127.107505);
+    let marker = new kakao.maps.Marker({
+      // 마커 생성 
+      position : markerPoint
+    });
+    // 마커 지도 위 표시.
+    marker.setMap(club_map);
+  });
+
+
+
+
+};
