eb595623c6c6d68d9180c3f2c2631abee48a1a29
JHS <118969653+ARISTAHS@users.noreply.github.com>
2024-10-11 17:58:27 +0900

    ROUTER 경로 테스트 확인

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from './components/layout';
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+import Login from "./routes/login";
+import CreateAccount from "./routes/create-account";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element : <Layout></Layout>
+    element : <Layout></Layout>,
+    children:[
+      {
+        path:"",
+        element:<Home/>,       
+      },
+      {
+        path: "profile",
+        element:<Profile/>
+      }
+    ]
+  },
+  //Layout 하위요소에 두지않고 로그인은 별도 라우팅 분리
+  {
+    path: "/login",
+    element:<Login/>
+  },
+  {
+    path:"/create-account",
+    element:<CreateAccount/>
   }
+
 ])
 
 function App() {
@@ -13,7 +37,7 @@ function App() {
 
   return (
     <>
-      
+      <RouterProvider router={router}/>
     </>
   )
 }
