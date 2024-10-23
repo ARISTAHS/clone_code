6c69594ae21668366825159a3e99b0f7fa3258ee
JHS <118969653+ARISTAHS@users.noreply.github.com>
2024-10-23 18:03:52 +0900

    firebase npm 설치 및 기본세팅

---
+++ src/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyChvhn6QfghOGmBsTngJJmlEb3m5-R9Z8E",
+  authDomain: "nwitter-reloaded-7f9c4.firebaseapp.com",
+  projectId: "nwitter-reloaded-7f9c4",
+  storageBucket: "nwitter-reloaded-7f9c4.appspot.com",
+  messagingSenderId: "692316540366",
+  appId: "1:692316540366:web:be029fc895449973462f9f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
